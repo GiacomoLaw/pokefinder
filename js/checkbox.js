490b2394c7bfc35693430c84d776190d78243df2
Giacomo Lawrance <GiacomoLaw@users.noreply.github.com>
2017-03-16 14:03:04 +0000

    Added master comment

--- js/checkbox.js
+++ js/checkbox.js
@@ -1,3 +1,5 @@
+// these checkers check the checkbox value, and then check all the checkboxs with the same value
+
 // rattata checker
 $("input[value='rattata'][type='checkbox']").change(function(){	
 	$("input[value='rattata'][type='checkbox']").prop("checked", $(this).prop("checked"));
