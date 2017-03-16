0fd85469399ebf074b11000c6f3273debee5430e
Giacomo Lawrance <GiacomoLaw@users.noreply.github.com>
2017-03-16 14:01:22 +0000

    Added pikipek checker

--- js/checkbox.js
+++ js/checkbox.js
@@ -2,3 +2,8 @@
 $("input[value='rattata'][type='checkbox']").change(function(){	
 	$("input[value='rattata'][type='checkbox']").prop("checked", $(this).prop("checked"));
 });
+
+// pikipek checker
+$("input[value='pikipek'][type='checkbox']").change(function(){	
+	$("input[value='pikipek'][type='checkbox']").prop("checked", $(this).prop("checked"));
+});
