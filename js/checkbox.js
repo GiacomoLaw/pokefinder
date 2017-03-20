9ba4add15925f1192e69c485a2ca98e11f3fca9e
Giacomo Lawrance <GiacomoLaw@users.noreply.github.com>
2017-03-20 14:15:07 +0000

    added more checkers

--- js/checkbox.js
+++ js/checkbox.js
@@ -14,3 +14,38 @@ $("input[value='pikipek'][type='checkbox']").change(function(){
 $("input[value='yungoos'][type='checkbox']").change(function(){	
 	$("input[value='yungoos'][type='checkbox']").prop("checked", $(this).prop("checked"));
 });
+
+// caterpie checker
+$("input[value='caterpie'][type='checkbox']").change(function(){	
+	$("input[value='caterpie'][type='checkbox']").prop("checked", $(this).prop("checked"));
+});
+
+// ledyba checker
+$("input[value='ledyba'][type='checkbox']").change(function(){	
+	$("input[value='ledyba'][type='checkbox']").prop("checked", $(this).prop("checked"));
+});
+
+// spinarak checker
+$("input[value='spinarak'][type='checkbox']").change(function(){	
+	$("input[value='spinarak'][type='checkbox']").prop("checked", $(this).prop("checked"));
+});
+
+// metapod checker
+$("input[value='metapod'][type='checkbox']").change(function(){	
+	$("input[value='metapod'][type='checkbox']").prop("checked", $(this).prop("checked"));
+});
+
+// pichu checker
+$("input[value='pichu'][type='checkbox']").change(function(){	
+	$("input[value='pichu'][type='checkbox']").prop("checked", $(this).prop("checked"));
+});
+
+// grubbin checker
+$("input[value='grubbin'][type='checkbox']").change(function(){	
+	$("input[value='grubbin'][type='checkbox']").prop("checked", $(this).prop("checked"));
+});
+
+// bonsly checker
+$("input[value='bonsly'][type='checkbox']").change(function(){	
+	$("input[value='bonsly'][type='checkbox']").prop("checked", $(this).prop("checked"));
+});
