06bb2c23b9a914f68eac5a236289414cf7630c6f
Peter Bronkalla <peterbronkalla@googlemail.com>
2017-07-10 15:32:54 +0200

    Improve the back to top button (#9)
    Thanks for this - was looking how to do this!

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	var offset = 300;
+	var animating = false;
+	var backToTop = $('.back-to-top');
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > offset) {
+			if (!animating) {
+				backToTop.fadeIn();
+			}
+		} else {
+			if (!animating) {
+				backToTop.fadeOut();
+			}
+		}
+	});
+
+	backToTop.click(function() {
+		animating = true;
+		$('body,html').animate({
+			scrollTop: 0
+		}, 500, function() {
+			animating = false;
+			backToTop.fadeOut();
+		});
+	});
+});
